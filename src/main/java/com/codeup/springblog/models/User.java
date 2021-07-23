b9e64ab8ba2225ff55cf4f88c66fc303e91fed72
Luke Curran <luke.peter.curran54@gmail.com>
2021-07-23 09:40:41 -0500

    friday push

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -18,15 +18,19 @@ public class User {
     private String password;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Ad> ads;
+    @OneToMany(cascade=CascadeType.ALL, mappedBy="user")
+    private List<Post> posts;
 
     public User() {
     }
 
-    public User(long id, String username, String email, String password) {
+    public User(long id, String username, String email, String password, List<Ad> ads, List<Post> posts) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.ads = ads;
+        this.posts = posts;
     }
 
     public long getId() {
@@ -67,6 +71,13 @@ public class User {
 
     public void setAds(List<Ad> ads) {
         this.ads = ads;
+    }
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
 
