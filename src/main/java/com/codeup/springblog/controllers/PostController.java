e46e89f6c470a4124c91b02ccb766d17bec5d8fa
Luke Curran <luke.peter.curran54@gmail.com>
2021-07-26 19:10:40 -0500

    word to ya motha

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,23 +1,58 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String viewPosts(){
-        return "View all posts.";
+    public String viewPosts(Model model) {
+        model.addAttribute("posts", postDao.findAll());
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String singlePost(@PathVariable long id){
-        return "View an individual Post.";
+    public String singlePost(@PathVariable long id, Model model) {
+        model.addAttribute("post", postDao.getById(id));
+        return "posts/show";
     }
 
+
+    @GetMapping("/posts/edit/{id}")
+    public String editForm(@PathVariable long id, Model model) {
+        model.addAttribute("post", postDao.getById(id));
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/edit/{id}")
+    public String editPost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
+        Post post = postDao.getById(id);
+        post.setTitle(title);
+        post.setBody(body);
+        postDao.save(post);
+        return "redirect:/posts/" + id;
+    }
+
+    @PostMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id) {
+        postDao.delete(postDao.getById(id));
+        return "redirect:/posts";
+    }
+
+
+    // When you visit the URL you will see the form to create a post.
     @GetMapping("/posts/create")
     @ResponseBody
     public String createForm(){
@@ -33,10 +68,5 @@ public class PostController {
         return "Creates new post.";
     }
 
-    @PostMapping("/posts/edit/{id}")
-    public String editPost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
-        Post post = postDao.getById(id);
-        post.setTitle(title);
-        post.setBody(body);
-    }
+
 }
